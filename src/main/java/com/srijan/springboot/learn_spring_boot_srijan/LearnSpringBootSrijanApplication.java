2136fb2982aa7142ba7d0ce128cefe401c2a47cf
srijan_singh <singhsrijan809@gmail.com>
2024-06-14 10:12:25 +0530

    First Commit

---
+++ src/main/java/com/srijan/springboot/learn_spring_boot_srijan/LearnSpringBootSrijanApplication.java
@@ -0,0 +1,14 @@
+package com.srijan.springboot.learn_spring_boot_srijan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootSrijanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootSrijanApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
